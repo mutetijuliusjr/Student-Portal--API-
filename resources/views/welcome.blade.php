1b75232fc561dce6fc568f2cd2403dc1f0dac778
Julius Muteti <mutetijuliusjr@gmail.com>
2022-05-20 13:53:35 +0300

    ⚠ Work in progress...

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
         <title>Laravel</title>
 
@@ -19,8 +20,11 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body class="antialiased">
+        <div id="example"></div>
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
